4b7e4960a7cdc090692879b8530f1e00e06f4f7e
kosinkadink <kosinkadink1@gmail.com>
2017-08-28 15:37:12 -0500

    Added most recent version of ScrapController, TODO put implementation-dependent constants in ScrapDefinitions to be edited for specific applications

---
+++ Arduino/libraries/ScrapController/ScrapDefinitions.h
@@ -0,0 +1,14 @@
+//
+// Created by Jedrzej Kosinski on 8/28/2017.
+//
+
+#ifndef SCRAPCONTROLLER_SCRAPDEFINITIONS_H_H
+#define SCRAPCONTROLLER_SCRAPDEFINITIONS_H_H
+
+
+// ScrapController constants
+// ScrapDualController constants
+// ScrapFullController constants
+
+
+#endif //SCRAPCONTROLLER_SCRAPDEFINITIONS_H_H
